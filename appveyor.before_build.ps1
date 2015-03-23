9c9d885b70653e7f78579ff4a080c997c75ff075
Daniel Rasmussen <development@dlras.net>
2015-03-23 13:42:59 -0500

    Added pre-build NuGet restore command via .ps.

---
+++ appveyor.before_build.ps1
@@ -0,0 +1,15 @@
+$source = "https://www.nuget.org/nuget.exe"
+$nugetFolder = $PSScriptRoot + "\.nuget"
+$nugetFile = $nugetFolder + "\nuget.exe"
+ 
+if(!(Test-Path -Path $nugetFile )){
+   New-Item -ItemType Directory -Force -Path $nugetFolder
+   Invoke-WebRequest $source -OutFile $nugetFile
+}
+
+Get-ChildItem $PSScriptRoot -Filter "*.sln" |
+   Where-Object { $_.Attributes -ne "Directory"} |
+      ForEach-Object {
+         Write-Host "Restoring NuGet packages for $_..."
+         & "$nugetFile" restore "$_"
+      }
