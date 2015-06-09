ad8ef661f1867670382744d134250cef1e3a5bbe
Daniel Rasmussen <development@dlras.net>
2015-06-09 09:24:34 -0500

    Cleaned up assembly info and unused script.

--- Library/Properties/AssemblyInfo.cs
+++ Library/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("BitFn.CoreUtilities.ProjectTemplate")]
 [assembly: AssemblyDescription("A template project for building class libraries with Visual Studio 2015, C# 6, NUnit 2, AutoFixtures/AutoMoq, and NuGet packaging using the Appveyor build platform.")]
 [assembly: AssemblyConfiguration("")]
@@ -13,24 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2b2fcf95-d2dd-46cc-b152-a13fe6c30525")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
--- appveyor.publish_environment.bat
+++
@@ -1,2 +0,0 @@
-set > environment.txt
-appveyor PushArtifact environment.txt
