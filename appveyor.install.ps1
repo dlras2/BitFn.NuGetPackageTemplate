6f28623b6b00ec3f90090d545d998cfeca026705
Daniel Rasmussen <development@dlras.net>
2015-06-10 14:04:37 -0500

    Refactored versioning.

--- appveyor.install.ps1
+++ appveyor.install.ps1
@@ -1,5 +1,5 @@
 # Regular expression for matching semantic versioning (http://semver.org/)
-$regex = "^v(?<version>(?<major>\d+)\.(?<minor>\d+)\.(?<revision>\d+))(?<prerelease>(?:\-[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*)?)(?<metadata>(?:\+[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*)?)$"
+$regex = "^v(?<version>(?<major>\d+)\.(?<minor>\d+)\.(?<revision>\d+))(?<prerelease>(?:\-[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*)?)(?<metadata>(?:\+[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*)?)($|(?=\s))"
 
 # Create link to build log
 $build_url = [string]::Format("{0}/project/{1}/{2}/build/{3}",
@@ -22,15 +22,19 @@ If (!$env:APPVEYOR_PULL_REQUEST_NUMBER -and ($env:APPVEYOR_REPO_BRANCH -eq "mast
 
   # Only update assembly version on breaking changes (major version increments)
   $env:ASSEMBLY_VERSION = $matches['major'] + ".0.0"
-  # File version includes NuGet release number plus build number
+  # File version includes release number plus build number
   $env:ASSEMBLY_FILE_VERSION = $matches['version'] + "." + $env:APPVEYOR_BUILD_NUMBER
+  # Assembly info version is equal to full semantic version plus build number metadata
+  $env:ASSEMBLY_INFORMATIONAL_VERSION = $matches['version'] + $matches['prerelease'] + $matches['metadata']
+  $delimiter = If (!!($matches['metadata'])) {"."} Else {"+"}
+  $env:ASSEMBLY_INFORMATIONAL_VERSION = $env:ASSEMBLY_INFORMATIONAL_VERSION + $delimiter + $env:APPVEYOR_BUILD_NUMBER
   # NuGet doesn't allow dots in prerelease tags (-) or any build metadata (+)
-  $env:ASSEMBLY_INFORMATIONAL_VERSION = $matches['version'] + ($matches['prerelease'] -replace "\.", "")
+  $env:PACKAGE_VERSION = $matches['version'] + ($matches['prerelease'] -replace "\.", "-")
 
   # Set up GitHub release properties
-  $env:PRERELEASE = !!($matches['prerelease']) -or ($matches['major'] -eq '0')
+  $env:PRERELEASE = (!!($matches['prerelease']) -or ($matches['major'] -eq '0'))
   $env:RELEASE_TAG = $env:APPVEYOR_REPO_COMMIT_MESSAGE
-  $env:RELEASE_TITLE = "Version " + ($env:APPVEYOR_REPO_COMMIT_MESSAGE).substring(1)
+  $env:RELEASE_TITLE = "Version " + $env:ASSEMBLY_INFORMATIONAL_VERSION
 
   # Set release notes
   $env:RELEASE_NOTES = $env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED
@@ -43,11 +47,12 @@ Else
   # Set assembly versions to "version-less" zero-versions
   $env:ASSEMBLY_VERSION = "0.0.0"
   $env:ASSEMBLY_FILE_VERSION = "0.0.0." + $env:APPVEYOR_BUILD_NUMBER
+  $env:ASSEMBLY_INFORMATIONAL_VERSION = "0.0.0+" + ($env:APPVEYOR_REPO_BRANCH -replace "[^0-9A-Za-z-]", "") + "." + $env:APPVEYOR_BUILD_NUMBER
   # NuGet doesn't allow dots in prerelease tags (-) or any build metadata (+)
-  $env:ASSEMBLY_INFORMATIONAL_VERSION = "0.0.0-" + ($env:APPVEYOR_REPO_BRANCH -replace "[^0-9A-Za-z]", "") + "-" + $env:APPVEYOR_BUILD_NUMBER
+  $env:PACKAGE_VERSION = "0.0.0-" + ($env:APPVEYOR_REPO_BRANCH -replace "[^0-9A-Za-z]", "") + "-" + $env:APPVEYOR_BUILD_NUMBER
 
   # Set release notes
-  $env:RELEASE_NOTES = [string]::Format("{0} [@{1}]({2})",
+  $env:RELEASE_NOTES = [string]::Format("{0}\n\n[@{1}]({2})",
     $env:APPVEYOR_REPO_COMMIT_MESSAGE, ($env:APPVEYOR_REPO_COMMIT).substring(0,7), $commit_url)
 }
 
@@ -57,6 +62,7 @@ $env:RELEASE_NOTES = [string]::Format("{0}\n\n[Build #{1}]({2})", $env:RELEASE_N
 # Patch all .nuspec files
 Get-ChildItem *.nuspec -Recurse | % {
   $xml = [xml](Get-Content $_)
+  $xml.SelectNodes("//version") | % {$_.InnerText = ($env:PACKAGE_VERSION) }
   $xml.SelectNodes("//releaseNotes") | % {$_.InnerText = ($env:RELEASE_NOTES).Replace("\n", "`r`n") }
   $xml.Save($_)
 }
@@ -64,3 +70,4 @@ Get-ChildItem *.nuspec -Recurse | % {
 Write-Host "Assembly version" $env:ASSEMBLY_VERSION
 Write-Host "Assembly file version" $env:ASSEMBLY_FILE_VERSION
 Write-Host "Assembly informational version" $env:ASSEMBLY_INFORMATIONAL_VERSION
+Write-Host "Package version" $env:PACKAGE_VERSION
