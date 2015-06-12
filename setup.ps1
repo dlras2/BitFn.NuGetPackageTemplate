ecd0550dfee0bb8f0599b5956b448a4cfeb73143
Daniel Rasmussen <development@dlras.net>
2015-06-12 09:29:37 -0500

    Updated setup script to replace file contents.

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,22 @@
-$old_name = "BitFn.NugetPackageTemplate"
-$name_regex = "^[0-9A-Za-z]+(\.[0-9A-Za-z]+)*$"
-
 # Gather variables
 Clear-Host
+$old_name = "BitFn.NugetPackageTemplate"
 $new_name = read-host "Solution/Library Name (e.g. BitFn.MyLibrary)"
+$name_regex = "^[0-9A-Za-z]+(\.[0-9A-Za-z]+)*$"
 While (!($new_name -match $name_regex))
 {
   Write-Host "Invalid name:" $new_name -foreground "red"
   $new_name = read-host "Solution/Library Name (e.g. BitFn.MyLibrary)"
 }
+$old_author = "Daniel Rasmussen"
+$new_author = read-host "Author/Company Name"
 
-# Prepare solution
-Rename-Item -path "$old_name.sln" -newname "$new_name.sln" -whatIf
-#Get-ChildItem *.cs -Recurse | % {
-#  Get-Content $_.PSPath |
-#  % { $_ -replace $old_name, $new_name } | 
-#  Set-Content $_.PSPath
-#}
+$OutputEncoding = [System.Text.Encoding]::UTF8
+# Rename solution
+Rename-Item -path "$old_name.sln" -newname "$new_name.sln"
+# Replace global changes
+Get-ChildItem . -Include *.cs, *.*proj, *.config, *.nuspec -Recurse | % {
+  (Get-Content $_.PSPath) |
+  % { $_ -replace "\b"+$old_name+"\b", $new_name -replace "\b"+$old_author+"\b", $new_author } |
+  Write-Output | Out-File $_.PSPath -encoding utf8
+}
